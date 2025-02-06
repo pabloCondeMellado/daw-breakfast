8c316f4c9f8e396ca3cd07cc94eaa92757cbb9eb
susana004 <susanamartinorozco050@gmail.com>
2025-02-06 17:44:16 +0100

    metodos faltantes

--- src/main/java/com/daw/persistence/crud/DesayunoCrudRepository.java
+++ src/main/java/com/daw/persistence/crud/DesayunoCrudRepository.java
@@ -11,5 +11,5 @@ public interface DesayunoCrudRepository extends ListCrudRepository<Desayuno, Int
 	List<Desayuno> findDesayunoEstablecimientoPuntuacion(Integer idEstablecimiento);
 	List<Desayuno> findDesayunoEstablecimientoPrecio(Integer idEstablecimiento);
 	List<Desayuno> findDesayunosPorPuntuacion();
-
+ 
 }
